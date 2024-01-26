1b4ce795359045e6b34906f8e945dc3a54308468
Thaitran <tranthai6693@gmail.com>
2024-01-27 00:02:45 +0700

    update HEP

--- code/HEP.py
+++ code/HEP.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import Counter
 
 data = {
     'Tid': ['T1', 'T2', 'T3', 'T4', 'T5', 'T6'],
@@ -12,15 +13,49 @@ data = {
 df = pd.DataFrame(data)
 df['Item_Length'] = df['Items'].apply(lambda items: len(items))
 
-unique_items = df['Items'].explode().unique()
+unique_items = sorted(df['Items'].explode().unique())
 
-result = {}
+occupancy_list = {}
 for item in unique_items:
     tid_list = df[df['Items'].apply(lambda items: item in items)]['Tid'].tolist()
     tid_lengths = [(tid, len(df[df['Tid'] == tid]['Items'].iloc[0])) for tid in tid_list if item in df[df['Tid'] == tid]['Items'].iloc[0]]
-    result[item] = tid_lengths
-
-print(result)
+    occupancy_list[item] = tid_lengths
     
+stset = {}
+for key, values in occupancy_list.items():
+    stset[key] = [item[0] for item in values]
+
+support = {}
+support = {key: len(value) for key, value in stset.items()}
+
+occupancy = {}
+for key, values_list in occupancy_list.items():
+    total = 0
+    for i in values_list:
+        total += len(key)/i[1]
+    occupancy[key] = round(total, 2)
+
+df_occupancy = pd.DataFrame(occupancy.items(), columns=['Item', 'O(P)'])
+df_support = pd.DataFrame(support.items(), columns=['Item', 'Sup(P)'])
+
+merge_df = df_support.merge(df_occupancy, on = "Item")
+
+# for key, values in occupancy_list.items():
+#     print(f"{key}: {values}")
+
+prepare_UBO = {}
+
+for key, list_values in occupancy_list.items():
+    values = [i[1] for i in list_values]
+    l_key = sorted(set(values))
+    
+    counter = Counter(values)
+    n_key = [counter[i] for i in l_key]
+    
+    prepare_UBO[key] = {'l({})'.format(key): l_key, 'n({})'.format(key): n_key}
+    
+for key, values in prepare_UBO.items():
+    print(f"{key}: {values}")
+
 
 
