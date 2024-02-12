fe2ff35f809c9e79f85b3426706da7efdacd83ed
Thaitran <tranthai6693@gmail.com>
2024-02-13 01:36:36 +0700

    update

---
+++ code/fruithut/fruithut.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = []
+with open(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\fruithut\fruithut.txt', 'r') as file:
+    lines = file.readlines()
+    for i, line in enumerate(lines):
+        tid = f"T{i+1}"
+        items = line.strip().split(' ')
+        data.append([tid, items])
+
+df = pd.DataFrame(data, columns=['Tid', 'Items'])
+df = df.head(100)
+print(df)
+# df.to_csv(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\fruithut.csv')
