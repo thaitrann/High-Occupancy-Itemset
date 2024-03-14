650507ce6b507a1697fd96a4f4cf0ddcde02033d
Thaitran <tranthai6693@gmail.com>
2024-03-15 03:28:06 +0700

    update

--- code/foodmart/foodmart.py
+++ code/foodmart/foodmart.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 data = []
-with open(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\fruithut\fruithut.txt', 'r') as file:
+with open(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\foodmart\foodmart.txt', 'r') as file:
     lines = file.readlines()
     for i, line in enumerate(lines):
         tid = f"T{i+1}"
@@ -10,5 +10,5 @@ with open(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\f
 
 df = pd.DataFrame(data, columns=['Tid', 'Items'])
 # df = df.head(10000)
-print(len(df))
+print(df)
 # df.to_csv(r'E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\fruithut.csv')
