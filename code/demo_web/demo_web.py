c1d3471b32e0803ee7ea7ed15c8bef8d86e25639
Thaitran <tranthai6693@gmail.com>
2024-03-18 03:19:12 +0700

    update

--- code/demo_web/demo_web.py
+++ code/demo_web/demo_web.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\demo_web\demo_web.csv")
+df = pd.read_csv("E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\demo_web\demo_web_127.csv")
 df['Items'] = df['Items'].apply(lambda x: x.split(','))
 df['Items'] = df['Items'].apply(lambda x: list(set(x)))
 if __name__ == "__main__":
@@ -8,4 +8,4 @@ if __name__ == "__main__":
         # Lấy danh sách các dòng có phần tử bị trùng lặp trong cột "Items"
     duplicated_rows = df[df['Items'].apply(lambda x: len(set(x)) != len(x))]
     print(len(duplicated_rows))
-    df.to_csv("E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\demo_web\demo_web_remove_dup.csv")
+    # df.to_csv("E:\MyDesktop\ThaiTran\Personal_Project\High-Occupancy-Itemset\code\demo_web\demo_web_remove_dup.csv")
